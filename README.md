df14312e52b3b4fc10c1eb47be7e34c070c99dea
Deepak <deepakrajan.grea@gmail.com>
2021-03-12 01:11:23 +0530

    arugment

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # My first cli tool
 ## Based on
 [[https://trstringer.com/easy-and-nice-python-cli/]]
+
+When you add a new package install `sudo pip3 install -e .`
--- cli/__main__.py
+++ cli/__main__.py
@@ -1,15 +1,19 @@
 import sys
-# from .classmodule import MyClass
-# from .funcmodule import my_function
+from .classmodule import MyClass
+from .welcome import wel
 
 def main():
-    print('in main')
     args = sys.argv[1:]
-    print('count of args :: {}'.format(len(args)))
-    for arg in args:
-        print('passed argument :: {}'.format(arg))
-
-    # my_function('hello world')
+    # print(args)
+    try:
+        if args[0] == "deepak":
+            try:
+                if args[1] == "-h":
+                    print("no help command has been set yet")
+            except:
+                wel("Dupeeer")
+    except:
+        print('Expected atleast one argument.')
 
     # my_object = MyClass('Thomas')
     # my_object.say_name()
